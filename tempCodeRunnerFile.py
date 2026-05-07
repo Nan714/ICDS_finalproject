211000ac771cee9e03bed3ab2dd313c1ebb31a2c
Nan <nanw70104@gmail.com>
2026-05-07 10:34:13 +0800

    init

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-        # messages = [
-        #     {"role": "system", "content": f"Enter role play mode. You are {self.name}, a professional academic advisor at NYU Shanghai. Reply warmly, within 20 words."},
-        #     {"role": "user", "content": query + "/no_think"},
-        # ]
